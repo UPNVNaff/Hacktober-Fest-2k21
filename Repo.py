90758a939cf6e254ccecc54261d38a9df6ee9e91
UPNVNaff <90653936+UPNVNaff@users.noreply.github.com>
2021-09-18 20:02:24 +0700

    Nama Participant
    Tambahin disini

---
+++ Repo.py
@@ -0,0 +1,10 @@
+# List Nama
+list_nama = [
+    ["Agus", "Asep", "Adinda"],
+    ["Putri", "Steven", "Dadang"],
+    ["Kevin", "Mutiara", "Clara"]
+]
+print ("Nama participant : ")
+for participant in list_nama:
+    for nama in participant:
+        print (nama)
